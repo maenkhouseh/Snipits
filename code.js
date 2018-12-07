239086a6d88e9287ca0e8429cc97899300d96764
maenkhouseh <33247942+maenkhouseh@users.noreply.github.com>
2018-12-07 13:31:09 +0400

    Update code.js

--- code.js
+++ code.js
@@ -5,3 +5,28 @@ function sleep(miliseconds) {
    var currentTime = new Date().getTime();
    while (currentTime + miliseconds >= new Date().getTime()) { }
 }
+
+
+function insertAfter(referenceNode, newNode) { // this inserts DOM element after reference element
+     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+ }
+
+ function insertBefore(newelement, referenceNode) { // this inserts DOM element before reference element
+     referenceNode.parentNode.insertBefore(newelement, referenceNode);
+ }
+
+
+ let attr = select.options[select.selectedIndex].getAttribute('attr')
+
+ 
+ // conditional object properties
+ let userData = Object.assign({}, username !== '' ? {
+         username
+     } : null,
+     password !== '' ? {
+         password
+     }: null,
+     accountID !== '' ? {
+         accountID
+     } : null
+ );
