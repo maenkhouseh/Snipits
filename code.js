51cc401e4d8cf36bbe84abd6bf71c4f02251241b
maenkhouseh <33247942+maenkhouseh@users.noreply.github.com>
2018-12-29 11:13:22 +0400

    Update code.js

--- code.js
+++ code.js
@@ -36,3 +36,15 @@ var index = array.indexOf(item);
 if (index > -1) {
   array.splice(index, 1);
 }
+
+function getUrlParameter(sParam) {
+   var sPageURL = window.location.search.substring(1);
+   var sURLVariables = sPageURL.split('&');
+   for (var i = 0; i < sURLVariables.length; i++) {
+      var sParameterName = sURLVariables[i].split('=');
+      if (sParameterName[0] == sParam) {
+         return sParameterName[1];
+      }
+   }
+}
+
