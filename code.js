8025fbbc07c8976da257ea1afd7f7481e26444d1
Dr. Maen K Househ <maen.househ@evotik.com>
2019-05-27 14:30:26 +0400

    Update code.js

--- code.js
+++ code.js
@@ -93,3 +93,35 @@ const JSONtoCSV = (arr, columns, delimiter = ',') =>
       )
     )
   ].join('\n');
+
+
+
+
+//Get all text nodes in a given container
+//Source: http://stackoverflow.com/a/4399718/560114
+function getTextNodesIn(node, includeWhitespaceNodes) {
+    var textNodes = [], nonWhitespaceMatcher = /\S/;
+
+    function getTextNodes(node) {
+        if (node.nodeType == 3) {
+            if (includeWhitespaceNodes || nonWhitespaceMatcher.test(node.nodeValue)) {
+                textNodes.push(node);
+            }
+        } else {
+            for (var i = 0, len = node.childNodes.length; i < len; ++i) {
+                getTextNodes(node.childNodes[i]);
+            }
+        }
+    }
+
+    getTextNodes(node);
+    return textNodes;
+}
+
+var textNodes = getTextNodesIn( $("#container")[0], false );
+var i = textNodes.length;
+var node;
+while (i--) {
+    node = textNodes[i];
+    node.textContent = node.textContent.replace(/Hi/g, "Hello");
+}
